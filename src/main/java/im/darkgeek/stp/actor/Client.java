be374e5d1708960d8fe4ba67d9986eac786510b2
Justin Yang <linuxjustin@gmail.com>
2015-11-05 00:56:53 +0800

    Init commit.

---
+++ src/main/java/im/darkgeek/stp/actor/Client.java
@@ -0,0 +1,11 @@
+package im.darkgeek.stp.actor;
+
+/**
+ * Created by justin on 15-11-4.
+ */
+public class Client
+    implements Runnable{
+    public void run() {
+
+    }
+}
---
+++ src/main/java/im/darkgeek/stp/connection/WebSocket.java
@@ -0,0 +1,7 @@
+package im.darkgeek.stp.connection;
+
+/**
+ * Created by justin on 15-11-5.
+ */
+public class WebSocket {
+}
