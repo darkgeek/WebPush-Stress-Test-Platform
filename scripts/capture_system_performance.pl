7284260f83e21cd94176ac60193ece3fdd5f35ad
Justin Yang <linuxjustin@gmail.com>
2015-11-11 00:27:15 +0800

    Tune on script to capture system performance.

--- scripts/capture_system_performance.pl
+++ scripts/capture_system_performance.pl
@@ -2,7 +2,19 @@
 use strict;
 use 5.010;
 use warnings;
+
+use threads;
+use threads::shared;
+
 use Data::Dumper;
+use IO::Socket;
+use Net::hostent;
+
+use constant PORT => 9002;
+my $stat_task;
+my $stop_task :shared = 0;
+my @stats;
+my $push_server_pid;
 
 sub get_current_stat_by_pid {
     my $pid = shift;
@@ -20,7 +32,43 @@ sub get_current_stat_by_pid {
             last;
         }
     }
+
     return $res;
+}
+
+sub stat_job {
+    my $client = shift;
+    say "Push server pid is $push_server_pid";
+    while (!$stop_task) {
+        push @stats, get_current_stat_by_pid($push_server_pid);
+        sleep(1);
+    }
+
+    say "stop this job!";
+    print Dumper(@stats);
+
+    my $cal_res = calculate();
+    print $client $cal_res->{cpu}."-".$cal_res->{mem};
+    close $client;
+}
+
+sub calculate {
+    my $cpu_total = 0;
+    my $mem_total = 0;
+    my $cal_res = {};
+
+    foreach my $stat (@stats) {
+        my $cpu = $stat->{cpu};
+        my $mem = $stat->{mem};
+        
+        $cpu_total += $cpu;
+        $mem_total += $mem;
+    }
+
+    $cal_res->{cpu} = $cpu_total * 1.0 / @stats * 1.0;
+    $cal_res->{mem} = $mem_total * 1.0 / @stats * 1.0;
+
+    return $cal_res;
 }
 
 sub clean_blank_item {
@@ -46,5 +94,39 @@ sub remove_item_in_array {
     splice(@$array, $index, 1);
 }
 
-my $stat_result = get_current_stat_by_pid(1054);
-print Dumper($stat_result);
+sub start_server {
+    my $server = IO::Socket::INET->new(Proto     => "tcp", 
+                                       LocalPort => PORT,
+                                       Listen    => SOMAXCONN,
+                                       Reuse     => 1);
+    die "can't setup server" unless $server;
+    say "[Server $0 accepting clients...]";
+
+    while (my $client = $server->accept()) {
+        $client->autoflush(1);
+        my $hostinfo = gethostbyaddr($client->peeraddr);
+        printf "[Connect from %s]\n", $hostinfo ? $hostinfo->name : $client->peerhost;
+        while ( <$client> ) {
+            next unless /\S/;       # blank line
+            if    (/quit|exit/i)    { last                                       }
+            elsif (/start [0-9]+/i)        {say "Start!"; start_stat_task($client, $_);}
+            elsif (/stop/i)         {say "Stop!"; stop_stat_task();}
+        }
+        close $client;
+    }
+}
+
+sub start_stat_task {
+    my $client = shift;
+    my $raw_cmd = shift;
+    my @params = split ' ', $raw_cmd;
+
+    $push_server_pid = $params[1];
+    $stat_task = threads->create(\&stat_job, $client) unless defined $stat_task;
+}
+
+sub stop_stat_task {
+    $stop_task = 1;
+}
+
+start_server();
